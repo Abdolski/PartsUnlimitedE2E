f238a7835a3baf6ef0d812ea805762c1daa99c11
Abdolski <abdolskibadawi@gmail.com>
2026-04-27 14:12:10 +0100

    added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // first commit ex
+        // second commit ex
         [Key]
         public int CartItemId { get; set; }
 
